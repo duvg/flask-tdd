92c1788dcd4e817ae4f984dfdf01287a224d8041
Duviel Garcia <duviel7@gmail.com>
2024-07-13 15:58:45 -0500

    Added database containaer
    Setup postgres database container for application

--- application/config.py
+++ application/config.py
@@ -1,5 +1,18 @@
+import os
+
 class Config(object):
     """Base Configuration"""
+    db_user = os.environ["POSTGRES_USER"]
+    db_password = os.environ["POSTGRES_PASSWORD"]
+    db_hostname = os.environ["POSTGRES_HOSTNAME"]
+    db_port = os.environ["POSTGRES_PORT"]
+    database_name = os.environ["APPLICATION_WEB"]
+    
+    
+    SQLALCHEMY_DATABASE_URL = (
+        f"postgressql+pysopg2://{db_user}:{db_password}@{db_hostname}:{db_port}/{database_name}"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class ProductionConfig(Config):
